84d7d217b41aece795f4be29696e5cd728742d8e
oussama2018 <oussamakmar1@gmail.com>
2024-10-03 22:49:19 +0100

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart,signInFailure,signInSuccess } from '../redux/user/userSlice';
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading, error} =useSelector((state)=>state.user)
   const navigate = useNavigate();
+  const  dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -17,7 +18,7 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart())
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -27,16 +28,13 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message))
         return;
       }
-      setLoading(false);
-      console.log(data); 
+      dispatch(signInSuccess(data))
       navigate('/'); 
     } catch (error) {
-      setLoading(false)
-      setError(error.message)
+      dispatch(signInFailure(signInFailure(error.message)))
     }
     
   };
@@ -44,7 +42,7 @@ export default function SignIn() {
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type="email"
