612b7ee99eab80ecc1980647d56f62e22eddbe1b
oussama2018 <oussamakmar1@gmail.com>
2024-10-03 23:10:32 +0100

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+// Correction : Utilisation d'une nouvelle variable pour éviter l'erreur de référence
+const persistedRootReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedRootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
